3349ce250e6b0f69ad88c82872bc7d5ee59a8d22
Masud karim <msmasud578@gmail.com>
2022-06-02 19:04:11 -0800

    init commit

---
+++ src/main/java/com/ecommerce/invoice_restapi/InvoiceRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.invoice_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/invoice_restapi/InvoiceRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.invoice_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
